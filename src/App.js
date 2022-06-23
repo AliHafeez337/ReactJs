459c2afc75eca0e062c7aaa7f13a824ab9c92076
Ali Hafeez <alihafeez337@gmail.com>
2022-06-23 20:37:37 +0500

    lecture 117 - useReducer

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import Expenses from './components/Expenses/Expenses';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
 
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,211 @@
+import React, { useState, useEffect, useReducer } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const defaultEmailState = {
+  value: '',
+  isValid: true
+}
+
+const validateEmail = email => {
+  return email.includes('@');
+}
+
+const emailReducerFunction = (oldState, action) => {
+  console.log('oldEmailState', oldState, 'action-type', action);
+  if (action) {
+    if (action.type === 'INPUT') {
+      return {
+        value: action.value,
+        isValid: validateEmail(action.value)
+      }
+    } else if (action.type === 'BLUR') {
+      return {
+        value: oldState.value,
+        isValid: validateEmail(oldState.value)
+      }
+    }
+  }
+
+  return defaultEmailState;
+}
+
+const defaultPasswordState = {
+  value: '',
+  isValid: true
+}
+
+const validatePassword = password => {
+  return password.trim().length > 6;
+}
+
+const passwordReducerFunction = (oldState, action) => {
+  console.log('oldPasswordState', oldState, 'action-type', action);
+  if (action) {
+    if (action.type === 'INPUT') {
+      if (action.value === oldState.value) {
+        return oldState;
+      }
+      return {
+        value: action.value,
+        isValid: validatePassword(action.value)
+      }
+    } else if (action.type === 'BLUR') {
+      return {
+        value: oldState.value,
+        isValid: validatePassword(oldState.value)
+      }
+    }
+  }
+
+  return defaultPasswordState;
+}
+
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [userEmail, setUserEmail] = useReducer(
+    emailReducerFunction,
+    defaultEmailState
+  );
+
+  const [userPassword, setUserPassword] = useReducer(
+    passwordReducerFunction,
+    defaultPasswordState
+  );
+
+  useEffect(() => {
+    console.log('userEmail', userEmail)
+  }, [userEmail])
+
+  useEffect(() => {
+    console.log('userPassword', userPassword)
+  }, [userPassword])
+
+  useEffect(() => {
+    console.log('EFFECT RUNNING');
+
+    return () => {
+      console.log('EFFECT CLEANUP');
+    };
+  }, []);
+
+  /**
+   * 
+   * "DEBOUNCING"
+   * the cleanup function will run before the actual useEffect runs
+   * in cleanup, we clear the old timer
+   * after user stops for 500ms, the callback inside timer will run that will perform resouce intensive task
+   * 
+   */
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form validity!');
+  //     setFormIsValid(
+  //       validateEmail(enteredEmail) && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+
+  //   return () => {
+  //     console.log('CLEANUP');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
+
+  const emailChangeHandler = (event) => {
+    // setEnteredEmail(event.target.value);
+    setUserEmail({
+      type: 'INPUT',
+      value: event.target.value
+    });
+
+    // setFormIsValid(
+    //   validateEmail(event.target.value) && validatePassword(enteredPassword)
+    // );
+    setFormIsValid(
+      validateEmail(event.target.value) && validatePassword(userPassword.value)
+    );
+  };
+
+  const passwordChangeHandler = (event) => {
+    // setEnteredPassword(event.target.value);
+    setUserPassword({
+      value: event.target.value,
+      type: 'INPUT'
+    })
+
+    // setFormIsValid(
+    //   validateEmail(enteredEmail) && validatePassword(event.target.value)
+    // );
+    setFormIsValid(
+      validateEmail(userEmail.value) && validatePassword(event.target.value)
+    );
+  };
+
+  const validateEmailHandler = () => {
+    // setEmailIsValid(validateEmail(enteredEmail));
+    setUserEmail({
+      type: 'BLUR'
+    });
+  };
+
+  const validatePasswordHandler = () => {
+    // setPasswordIsValid(validatePassword(enteredPassword));
+    setUserPassword({
+      type: 'BLUR'
+    })
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(userEmail.value, userPassword.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            userEmail.isValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={userEmail.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            userPassword.isValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={userPassword.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
